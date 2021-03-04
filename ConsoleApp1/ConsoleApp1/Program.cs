ac3504372b9c5ea126fbbe52b22c7f6b80098c42
TomaPartridge <80053439+TomaPartridge@users.noreply.github.com>
2021-03-05 01:27:21 +1000

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6 + 3;
+            Console.WriteLine("Happy End");
+            Console.ReadKey();
+        }
+    }
+}
